d480f0d731dbbc76b3c286d9f26ee528b10debbe
Juan Pablo Gomez Antonaz <juanpablo.gomez@globant.com>
2019-10-21 15:37:46 -0300

    Add admin

---
+++ db/migrate/20191021182619_create_admin_users.rb
@@ -0,0 +1,14 @@
+class CreateAdminUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :admin_users do |t|
+      t.string :name, null: false
+      t.string :last_name, null: false
+      t.string :email, null: false
+      t.string :phone
+      t.string :address
+      t.belongs_to :user, index: { unique: true }, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
