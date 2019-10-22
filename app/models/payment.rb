afb991e3e1467784beaa6db1e86791908f57fe97
Juan Pablo Gomez Antonaz <juanpablo.gomez@globant.com>
2019-10-22 19:03:44 -0300

    fix payment

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -4,13 +4,14 @@ class Payment < ApplicationRecord
   validates :amount, numericality: { greater_than: 0 }
   validates :date, presence: true
   validates :invoice, presence: true
-  validates :payment_method, presence: true
+  # validates :payment_method, presence: true
 
-  before_save :default_values
+  before_validation :default_values
 
   private
 
   def default_values
     self.date ||= Time.now
+    self.payment_method = 'efectivo' unless self.payment_method.present?
   end
 end
