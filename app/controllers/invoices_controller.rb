7a57825001d13965fb09be70ee9896f8cbc360b5
mberrueta <matiasberrueta@gmail.com>
2019-10-22 19:26:06 -0300

    fix?4

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -36,6 +36,6 @@ class InvoicesController < ApplicationController
     invoices = Invoice.all.includes(items: :service)
     invoices = invoices.where(student_id: params[:student_id]) if params[:student_id]
     invoices = invoices.joins(:student).where(students: { holder_id: params[:holder_id] }) if params[:holder_id]
-    invoices.to_json(include: items: :service, methods: [:subtotal, :total])
+    invoices.to_json(include: { items: :service }, methods: [:subtotal, :total])
   end
 end
