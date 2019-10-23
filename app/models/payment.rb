4c5fcd93e19780a8ea7bf764d7c7fb135fec9ff5
mberrueta <matiasberrueta@gmail.com>
2019-10-23 12:26:38 -0300

    added over payed restriction

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -4,7 +4,7 @@ class Payment < ApplicationRecord
   validates :amount, numericality: { greater_than: 0 }
   validates :date, presence: true
   validates :invoice, presence: true
-  # validates :payment_method, presence: true
+  validate :over_payments
 
   before_validation :default_values
 
@@ -13,5 +13,20 @@ class Payment < ApplicationRecord
   def default_values
     self.date ||= Time.now
     self.payment_method = 'efectivo' unless payment_method.present?
+  end
+
+  def over_payments
+    return unless invoice&.total < other_similar_payments.sum(:amount) + amount
+
+    if amount > invoice&.total
+      errors.add(:over_payed, "can't create a payment. Amount should be less than #{invoice&.total}")
+    else
+      left_pay = invoice&.total - other_similar_payments.sum(:amount)
+      errors.add(:over_payed, "can't create a payment. there are already #{other_similar_payments.count} payments. It left only $ #{left_pay} to pay")
+    end
+  end
+
+  def other_similar_payments
+    @other_similar_payments ||= Payment.where(invoice_id: invoice_id)
   end
 end
